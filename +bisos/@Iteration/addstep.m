6b927b1cb297676a28606005933f575a40e3db38
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2021-04-19 19:01:37 +0200

    Keep objective always as last step.

--- +bisos/@Iteration/addstep.m
+++ +bisos/@Iteration/addstep.m
@@ -103,6 +103,10 @@ J = arrayfun(@(sj) ~isempty(intersect(step.varout,sj.varin)), obj.steps);
 obj.stepgraph = addedge(obj.stepgraph, nodes(I), N+1);
 obj.stepgraph = addedge(obj.stepgraph, N+1, nodes(J));
 
-obj.steps(N+1) = step;
+% objective should be last step
+obj.steps(N+1) = obj.steps(N); 
+obj.steps(N) = step;
+
+obj.stepgraph = reordernodes(obj.stepgraph,[1:N-1 N+1 N]);
 
 end
