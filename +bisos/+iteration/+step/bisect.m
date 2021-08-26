739145f87fac3c274de3fc5c2bafeadac18cf640
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2021-08-26 12:49:51 +0200

    Modify initial guesses for bisection variable.

--- +bisos/+iteration/+step/bisect.m
+++ +bisos/+iteration/+step/bisect.m
@@ -28,7 +28,31 @@ methods
     
     function stepsol = solve(~,sosc,objective,sosoptions)
         % Solve quasi-convex optimization step.
+        
+        
         stepsol = goptimize(sosc,objective,sosoptions);
+    end
+    
+    function [sol,iter,stop] = run(step,prob,iter,sol,symbols,assigns,options)
+        % Overwriting OptStep#run
+        tlb = options.sosoptions.minobj;
+        tub = options.sosoptions.maxobj;
+        
+        if iter > 1 && max(length(tlb),length(tub)) == 1
+            % recover last solution
+            obj0 = double(bisos.subs(step.objective,symbols,sol,step.ovar));
+
+            % initial guesses for lower and upper bounds
+            tlb = obj0+tlb;
+            tub = obj0+tub;
+        end
+        
+        % set lower and upper bounds
+        options.sosoptions.minobj = tlb;
+        options.sosoptions.maxobj = tub;
+        
+        % run step as usual
+        [sol,iter,stop] = run@bisos.iteration.OptStep(step,prob,iter,sol,symbols,assigns,options);
     end
 end
 
